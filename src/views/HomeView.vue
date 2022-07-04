1c682f9f813efb6d97d8b79a0503113a68822dd4
Kashif Ahmad <ahmadkashif773@gmail.com>
2022-07-04 22:39:45 +0500

    Project Refactoring

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    Hello world
   </main>
 </template>
+<script>
+  export default {
+
+  }
+</script>
