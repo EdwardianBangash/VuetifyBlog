de71a4ad025d9fc6419bcf7ab4bb8fd46fa7e1f6
Kashif Ahmad <ahmadkashif773@gmail.com>
2022-07-05 22:13:46 +0500

    Blogs

--- src/components/TheBlogs.vue
+++ src/components/TheBlogs.vue
@@ -1,5 +1,9 @@
 <template>
-    blogs
+    <div class="blog-wrapper">
+        <div class="blog-title">Hello world</div>
+        <div class="blog-desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, exercitationem!</div>
+        <div class="blog-action">Read More</div>
+    </div>
 </template>
 
 <script>
