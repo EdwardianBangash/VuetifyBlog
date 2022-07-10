b74fd81f22424ccc12d5b7ea87b0c7ad458828f2
Kashif Ahmad <ahmadkashif773@gmail.com>
2022-07-10 22:52:55 +0500

    Navbar setup

--- src/components/layouts/TheNavbar.vue
+++ src/components/layouts/TheNavbar.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="navbar-wrapper">
-    <h2 href="/">Vuetifyblog</h2>
+    <router-link to="/" class="logo">Vuetifyblog</router-link>
     <div class="navbar">
       <ul v-for="cat in categories" :key="cat.id">
         <li>
           <a href="" class="link">{{ cat.name }}</a>
         </li>
       </ul>
+      <div>
+        <div class="logins-wrapper" v-if="isLoggedIn">
+          <router-link to="/login" class="link">Login</router-link>
+          <router-link to="/register" class="link">Register</router-link>
+        </div>
+        <div class="logins-wrapper" v-else>
+          <router-link to="/dashboard" class="link">Dashboard</router-link>
+          <button class="btn logout-btn" @click="logout">Logout</button>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="logins-wrapper" v-if="isLoggedIn">
-    <router-link to="/login" class="link">Login</router-link>
-    <router-link to="/register" class="link">Register</router-link>
-  </div>
-  <div class="logins-wrapper" v-else>
-    <router-link to="/dashboard">Dashboard</router-link>
-    <button class="btn logout-btn" @click="logout">Logout</button>
   </div>
 </template>
 
@@ -61,9 +63,11 @@ export default {
   padding: 20px 10px;
 }
 
-.navbar{
+.navbar {
   flex-grow: 2;
-  width: 100%;
+  min-width: 30%;
+  position: absolute;
+  right: 0;
 }
 
 ul {
@@ -71,15 +75,12 @@ ul {
   text-align: right;
 }
 
-li {
-  display: inline;
-}
-
 a.link {
   text-decoration: none;
   padding-left: 10px;
   color: #000;
   font-size: 1em;
+  float: left;
 }
 
 a.active {
@@ -87,10 +88,16 @@ a.active {
   padding: 15px 5px;
 }
 
-.logins-wrapper {
-  position: absolute;
-  right: 10px;
-  top: 70px;
+.logo {
+  width: 20%;
+  text-decoration: none;
+  font-size: 20px;
+  color: #000;
+}
+
+.logins-wrapper button {
+      margin-left: 5px;
+      margin-top: -2px;
 }
 
 .logout-btn {
